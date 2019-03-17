ff2f082a570906e14384de782162be1628565e16
pixelomer <37103802+pixelomer@users.noreply.github.com>
2019-03-17 12:47:07 +0300

    AutoLayout for TabView

--- SmartDepictions/DepictionTabView.mm
+++ SmartDepictions/DepictionTabView.mm
@@ -31,14 +31,67 @@
 	currentTabs = [[NSMutableArray alloc] init];
 	for (int i = 0, count = [currentTabNames count]; i < count; i++) {
 		NSString *tabName = currentTabNames[i];
-		DepictionTab *tab = [[DepictionTab alloc] initWithFrame:CGRectMake(
-			(UIScreen.mainScreen.bounds.size.width / count) * i,
-			0,
-			UIScreen.mainScreen.bounds.size.width / count,
-			self.contentView.frame.size.height
-		)];
+		DepictionTab *tab = [[DepictionTab alloc] init];
+		tab.translatesAutoresizingMaskIntoConstraints = NO;
 		[tab setTitle:tabName forState:UIControlStateNormal];
+		[tab sizeToFit];
 		[self.contentView addSubview:tab];
+		if (i > 0) {
+			[self.contentView addConstraints:@[
+				[NSLayoutConstraint
+					constraintWithItem:tab
+					attribute:NSLayoutAttributeWidth
+					relatedBy:NSLayoutRelationEqual
+					toItem:currentTabs[0]
+					attribute:NSLayoutAttributeWidth
+					multiplier:1.0
+					constant:0.0
+				],
+				[NSLayoutConstraint
+					constraintWithItem:tab
+					attribute:NSLayoutAttributeHeight
+					relatedBy:NSLayoutRelationEqual
+					toItem:currentTabs[0]
+					attribute:NSLayoutAttributeHeight
+					multiplier:1.0
+					constant:0.0
+				],
+				[NSLayoutConstraint
+					constraintWithItem:tab
+					attribute:NSLayoutAttributeLeading
+					relatedBy:NSLayoutRelationEqual
+					toItem:currentTabs[i - 1]
+					attribute:NSLayoutAttributeTrailing
+					multiplier:1.0
+					constant:0.0
+				]
+			]];
+		}
+		else {
+			NSDictionary *views = @{ @"tab" : tab };
+			[self.contentView addConstraints:[NSLayoutConstraint
+				constraintsWithVisualFormat:@"V:|[tab]|"
+				options:0
+				metrics:nil
+				views:views
+			]];
+			[self.contentView addConstraints:[NSLayoutConstraint
+				constraintsWithVisualFormat:@"H:|[tab]"
+				options:0
+				metrics:nil
+				views:views
+			]];
+			[views release];
+			[self.contentView addConstraint:[NSLayoutConstraint
+				constraintWithItem:tab
+				attribute:NSLayoutAttributeWidth
+				relatedBy:NSLayoutRelationEqual
+				toItem:self.contentView
+				attribute:NSLayoutAttributeWidth
+				multiplier:(1.0 / count)
+				constant:0.0
+			]];
+		}
 		[currentTabs addObject:tab];
 	}
 	_tabs = [tabs retain];
