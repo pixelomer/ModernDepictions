1c90a7d1168d7625b53336461d9e337ade92a174
pixelomer <37103802+pixelomer@users.noreply.github.com>
2019-03-26 08:02:13 +0300

    Removed @available macro

--- macros.h
+++ macros.h
@@ -14,11 +14,6 @@
 #define min(a, b) (a > b ? b : a)
 #define max(a, b) (a > b ? a : b)
 
-// Macro to disable availability checks. Replace 1 with 0 if you aren't having issues with it. The resulting build will only work on newer versions of iOS when the availability checks are disabled.
-#if 0
-#define available(args...) (YES).boolValue
-#endif
-
 // Macros to properly perform selectors
 #define PerformSelector(target, self, _cmd) class_getMethodImplementation(NSClassFromString(@#target), _cmd)(self, _cmd)
 #define PerformSelectorWithArgs(target, self, _cmd, args...) class_getMethodImplementation(NSClassFromString(@#target), _cmd)(self, _cmd, args)
